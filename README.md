b160a38c5419a28c4ee6ce933fa439252b73662e
colton-aerospike <cmarkle@aerospike.com>
2023-08-11 17:39:34 +0000

    fix readme

--- README.md
+++ README.md
@@ -3,17 +3,28 @@ Simple bash script to deploy the Aerospike Kubernetes Operator via OLM to a kube
 
 ## Usage
 ```bash
-Usage: ./setup_olm.sh -n CLUSTER_NAME -v K8S_VERSION [-o OLM_VERSION] [ -R ]
+Usage: ./setup_olm.sh -n CLUSTER_NAME -v K8S_VERSION [-o OLM_VERSION, -E] [ -R ]
 
 -n) EKS cluster name to attach to
 -v) Kubernetes version to use when attaching via gaiakube
--o) OLM version to install: Default v0.21.2
+-o) OLM version to install: (Default: v0.25.0)
+-A) Install and Configure Amazon cluster-autoscaler (Default: false)
+-E) Install OpenEBS Operator (Default: false)
 -R) Run cleanup to remove OLM, operators, and aerospike namespace
 ```
-
+## Basic Install
 ```bash
 ./setup_olm.sh -n colton -v 1.27
 ```
 
+## Deploy OLM and OpenEBS, then configure Amazon cluster-autoscaler
+```bash
+./setup_olm.sh -n colton -v 1.27 -E -A
+```
+
+## Remove OLM installation
+```bash
+./setup_olm.sh -n colton -v 1.27 -R
+``` 
 
 
--- setup_olm.sh
+++ setup_olm.sh
@@ -50,12 +50,48 @@ function deployOpenEBS {
 	kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml
 }
 
+function configureAmazonAutoscaler {
+	# Patch the deployment to add the cluster-autoscaler.kubernetes.io/safe-to-evict annotation to the Cluster Autoscaler pods with the following command.
+
+	kubectl patch -n kube-system deployment.apps/cluster-autoscaler --type "json" -p "[
+        { 'op': 'add', 'path': '/spec/template/metadata/annotations/cluster-autoscaler.kubernetes.io~1safe-to-evict', 'value': 'false' }
+        ]"
+
+	# Remove the last command in array to overwrite with our cluster name:
+	# spec:
+	#  containers:
+	#  - command
+	#    - ./cluster-autoscaler
+	#    - --v=4
+	#    - --stderrthreshold=info
+	#    - --cloud-provider=aws
+	#    - --skip-nodes-with-local-storage=false
+	#    - --expander=least-waste
+	#    - --node-group-auto-discovery=asg:tag=k8s.io/cluster-autoscaler/enabled,k8s.io/cluster-autoscaler/YOUR_CLUSTER_NAME_HERE
+	#    - --balance-similar-node-groups
+	#    - --skip-nodes-with-system-pods=false
+	kubectl patch -n kube-system deployment.apps/cluster-autoscaler --type "json" -p '[{"op": "remove", "path": "/spec/template/spec/containers/0/command/-1" }]'
+
+	# Add our cluster name option and last two parameters 
+	# --balance-similar-node-groups
+	# --skip-nodes-with-system-pods=false
+	kubectl patch -n kube-system deployment.apps/cluster-autoscaler --type "json" -p "[
+	{'op': 'add', 'path': '/spec/template/spec/containers/0/command/-', 'value': '--node-group-auto-discovery=asg:tag=k8s.io/cluster-autoscaler/enabled,k8s.io/cluster-autoscaler/${CLUSTER_NAME}'},
+	{'op': 'add', 'path': '/spec/template/spec/containers/0/command/-', 'value': '--balance-similar-node-groups'},
+	{'op': 'add', 'path': '/spec/template/spec/containers/0/command/-', 'value': '--skip-nodes-with-system-pods=false' },
+	]"
+	
+	# Set deployment image for our cluster version
+	kubectl set image deployment cluster-autoscaler -n kube-system cluster-autoscaler=registry.k8s.io/autoscaling/cluster-autoscaler:"v${K8S_VERSION}.0"
+}
+
 function usage {
         echo "Usage: $0 -n CLUSTER_NAME -v K8S_VERSION [-o OLM_VERSION, -E] [ -R ]"
         echo ""
         echo "-n) EKS cluster name to attach to"
         echo "-v) Kubernetes version to use when attaching via gaiakube"
 	echo "-o) OLM version to install: (Default: v0.25.0)"
+	echo "-A) Install and Configure Amazon cluster-autoscaler (Default: false)"
 	echo "-E) Install OpenEBS Operator (Default: false)"
 	echo "-R) Run cleanup to remove OLM, operators, and aerospike namespace"
 }
@@ -65,7 +101,7 @@ function exit_abnormal {
 }
 
 function parseArgs {
-	while getopts "n:v:o:RE" options; do
+	while getopts "n:v:o:ARE" options; do
                 case "${options}" in
                 n)
                         CLUSTER_NAME="${OPTARG}" # set $NAME to specified value.
@@ -80,6 +116,9 @@ function parseArgs {
 			;;
 		E)	
 			INSTALL_OPENEBS=1
+			;;
+		A) 
+			AUTOSCALER=1
 			;;
                 :) # If expected argument omitted:
                         echo "Error: -${OPTARG} requires an argument."
@@ -110,11 +149,12 @@ function main {
 	gaiakube "$K8S_VERSION" "$CLUSTER_NAME"
        	[ $? -ne 0 ] && echo "Failed to connect with gaiakube" && exit 5
 	[ $CLEANUP -eq 1 ] && cleanUp
-	[ $INSTALL_OPENEBS -eq 1 ] && deployOpenEBS
 	downloadOlm
 	installOperator
 	createNamespaceAndServiceAccount
 	createSecrets
+	[ $INSTALL_OPENEBS -eq 1 ] && deployOpenEBS
+	[ $AUTOSCALER -eq 1 ] && configureAmazonAutoscaler
 	kubectl get all -n aerospike
 }
 
@@ -123,6 +163,7 @@ K8S_VERSION=
 OLM_VERSION="v0.25.0"
 CLEANUP=0
 INSTALL_OPENEBS=0
+AUTOSCALER=0
 
 main "$@"
 
